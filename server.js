2f3a07f82721ceb85d5cc74da8b6e90d0d4e85cc
Jacob Ashworth <jeashwor@gmail.com>
2020-10-16 12:31:38 -0400

    added env variables to mongodb connection and express connection for Heroku deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+// Setup connection to MongoDB database
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 // routes
 app.use(require("./routes/api.js"));
